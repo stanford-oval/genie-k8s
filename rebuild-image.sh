daa25172c90b82f1e9468356f882d2738b046569
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-01-28 11:18:30 -0800

    rebuild-image: use docker not podman
    podman can be aliased to docker but not viceversa, and in Ubuntu
    they do not alias, which breaks the $(aws ecr get-login) incantation

--- rebuild-image.sh
+++ rebuild-image.sh
@@ -7,15 +7,15 @@ check_config "IMAGE COMMON_IMAGE genie_version thingtalk_version decanlp_version
 set -e
 set -x
 
-#podman build -t ${COMMON_IMAGE} \
+#docker build -t ${COMMON_IMAGE} \
 #  --build-arg THINGPEDIA_DEVELOPER_KEY=${THINGPEDIA_DEVELOPER_KEY} \
 #  -f Dockerfile.common .
-#podman push ${COMMON_IMAGE}
+#docker push ${COMMON_IMAGE}
 
-podman build -t ${IMAGE} \
+docker build -t ${IMAGE} \
   --build-arg COMMON_IMAGE=${COMMON_IMAGE} \
   --build-arg DECANLP_VERSION=${decanlp_version} \
   --build-arg THINGTALK_VERSION=${thingtalk_version} \
   --build-arg GENIE_VERSION=${genie_version} \
   .
-podman push ${IMAGE}
+docker push ${IMAGE}
