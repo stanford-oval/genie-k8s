78ebeba9f702e71ddabb3315f02fc380390b103f
Silei <silei@stanford.edu>
2020-03-03 12:10:17 -0800

    Use no manual, no bert when evaluate since annotation doesn't matter

--- evaluate-job.sh
+++ evaluate-job.sh
@@ -23,6 +23,6 @@ ls -al
 mkdir -p tmp
 export GENIE_TOKENIZER_ADDRESS=tokenizer.default.svc.cluster.local:8888
 export TZ=America/Los_Angeles
-make geniedir=/opt/genie-toolkit experiment=${experiment} owner=${owner} eval_set=${eval_set} "$@" ${experiment}/${eval_set}/${model}.results
+make geniedir=/opt/genie-toolkit experiment=${experiment} owner=${owner} eval_set=${eval_set} "$@" ${experiment}/${eval_set}/${model}.results process_schemaorg_flags= update_canonical_flags=--skip
 #cat model/*.results > ${experiment}-${dataset}-${model}.results
 aws s3 sync ${experiment}/${eval_set}/ s3://almond-research/${owner}/${workdir}/${experiment}/${eval_set}/
