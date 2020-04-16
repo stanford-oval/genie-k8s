272b2df5d0f875feba3f5d89d5cf742dd7dbcb2e
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-04-15 21:51:40 -0700

    generate-dataset: make parallelism configurable
    Too much parallelism, and we die of OOM. Too little, and it takes
    forever to finish the datasets.

--- generate-dataset-job.sh
+++ generate-dataset-job.sh
@@ -2,7 +2,7 @@
 
 . /opt/genie-toolkit/lib.sh
 
-parse_args "$0" "owner project experiment dataset" "$@"
+parse_args "$0" "owner project experiment dataset parallel" "$@"
 shift $n
 
 set -e
@@ -24,6 +24,6 @@ aws s3 sync s3://almond-research/${owner}/workdir/${project}/ .
 
 export GENIE_TOKENIZER_ADDRESS=tokenizer.default.svc.cluster.local:8888
 export TZ=America/Los_Angeles
-make -j3 geniedir=/opt/genie-toolkit experiment=${experiment} owner=${owner} "$@" datadir
+make "-j${parallel}" geniedir=/opt/genie-toolkit "experiment=${experiment}" "owner=${owner}" "$@" datadir
 
 aws s3 sync datadir/ s3://almond-research/${owner}/dataset/${project}/${experiment}/${dataset}/
