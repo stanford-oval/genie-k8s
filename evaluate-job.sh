1d58e86b8f6d5206380215b885b972574daec345
Sina <s.j.semnani@gmail.com>
2020-05-24 19:26:17 -0700

    Print evaluation results

--- evaluate-job.sh
+++ evaluate-job.sh
@@ -15,7 +15,7 @@ set -x
 # trap on_error ERR
 
 pwd
-aws s3 sync s3://almond-research/${owner}/workdir/${project} .
+aws s3 sync --include '${project}/' s3://almond-research/${owner}/workdir/${project} .
 mkdir -p ${experiment}/models
 aws s3 sync --exclude 'iteration_*.pth' --exclude '*_optim.pth' s3://almond-research/${owner}/models/${project}/${experiment}/${model}/ ${experiment}/models/${model}/
 
@@ -24,4 +24,5 @@ mkdir -p tmp
 export GENIE_TOKENIZER_ADDRESS=tokenizer.default.svc.cluster.local:8888
 export TZ=America/Los_Angeles
 make geniedir=/opt/genie-toolkit experiment=$experiment owner=$owner $experiment/eval/$model.results
+cat $experiment/eval/$model.results
 aws s3 sync $experiment/eval/ s3://almond-research/${owner}/workdir/${project}/$experiment/eval/
