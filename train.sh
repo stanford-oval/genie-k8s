72e8068260e462e2df09a825a7adcdcf6d338998
mehrad <mehrad@stanford.edu>
2020-06-09 19:18:11 -0700

    merge master branch

--- train.sh
+++ train.sh
@@ -4,8 +4,20 @@
 . lib.sh
 
 check_config "S3_BUCKET OWNER DATASET_OWNER IMAGE PROJECT"
-parse_args "$0" "experiment dataset dataset_owner=${DATASET_OWNER} model task=${TRAIN_TASK_NAME} load_from=None" "$@"
+parse_args "$0" "experiment dataset dataset_owner=${DATASET_OWNER} model task=${TRAIN_TASK_NAME} load_from=None num_gpus=1" "$@"
+
 shift $n
+
+if [ "$num_gpus" = "1" ]; then
+  GPU_NUM=1
+	GPU_TYPE="p3.2xlarge"
+else
+  GPU_NUM=$num_gpus
+	GPU_TYPE="p3.8xlarge"
+fi
+
+check_config "IAM_ROLE OWNER DATASET_OWNER IMAGE PROJECT GPU_NUM GPU_TYPE"
+
 
 JOB_NAME=${OWNER}-train-${experiment}-${model}
 cmdline="--s3_bucket ${S3_BUCKET} --owner ${OWNER} --dataset_owner ${dataset_owner} --task_name \"${task}\" --project ${PROJECT} --experiment $experiment --dataset $dataset --model $model --load_from \"${load_from}\" -- "$(requote "$@")
