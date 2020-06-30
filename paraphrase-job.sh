1cdf27031bff1251691473085e58701b1fd4da2c
Lucas Sato <lucasjks@gmail.com>
2020-06-30 18:02:33 -0300

    Release disk space before filtering in paraphrase

--- paraphrase-job.sh
+++ paraphrase-job.sh
@@ -166,6 +166,8 @@ else
 
   # paraphrasing was successful, so upload the intermediate files
   aws s3 sync output_dataset s3://${s3_bucket}/${dataset_owner}/dataset/${project}/${experiment}/${output_dataset}
+  echo "Uploaded intermediate files to s3. Deleting synthetic.txt from pod..."
+  rm output_dataset/synthetic.txt
 fi
 
 if [ "$skip_filtering" = true ] ; then
