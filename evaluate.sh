cff3eb5010271411c21882f38f83182341427982
Ryan Othniel Kearns <ryankearns25@gmail.com>
2020-07-23 11:59:53 -0400

    Add --eval_version flag to evaluate job

--- evaluate.sh
+++ evaluate.sh
@@ -3,12 +3,12 @@
 . config
 . lib.sh
 
-parse_args "$0" "experiment model model_owner eval_set=eval" "$@"
+parse_args "$0" "experiment model model_owner eval_set=eval eval_version" "$@"
 shift $n
 check_config "S3_BUCKET OWNER IMAGE PROJECT"
 
 JOB_NAME=${OWNER}-evaluate-${experiment}-${model_owner}-${model}
-cmdline="--s3_bucket ${S3_BUCKET} --owner ${OWNER} --project ${PROJECT} --experiment ${experiment} --model ${model} --model_owner ${model_owner} --eval_set ${eval_set} -- "$(requote "$@")
+cmdline="--s3_bucket ${S3_BUCKET} --owner ${OWNER} --project ${PROJECT} --experiment ${experiment} --model ${model} --model_owner ${model_owner} --eval_set ${eval_set} --eval_version ${eval_version} -- "$(requote "$@")
 
 set -e
 set -x
