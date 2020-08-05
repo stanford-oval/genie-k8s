a2a445af0797ba0a9264cc3322348bfe575ce046
Sina <s.j.semnani@gmail.com>
2020-08-05 01:33:02 -0700

    In evaluation, model_owner is set to owner by defualt

--- evaluate-job.sh
+++ evaluate-job.sh
@@ -10,14 +10,14 @@ set -x
 
 # on_error () {
  	# on failure ship everything to s3
-# 	aws s3 sync . s3://almond-research/${owner}/models/${project}/${experiment}/${model}/failed_eval/
+# 	aws s3 sync . s3://${s3_bucket}/models/${project}/${experiment}/${model}/failed_eval/
 # }
 # trap on_error ERR
 
 pwd
 aws s3 sync s3://${s3_bucket}/${owner}/workdir/${project} .
-#mkdir -p ${experiment}/models
-#aws s3 sync --exclude 'iteration_*.pth' --exclude '*_optim.pth' s3://almond-research/${owner}/models/${project}/${experiment}/${model}/ ${experiment}/models/${model}/
+# mkdir -p ${experiment}/models
+# aws s3 sync --exclude 'iteration_*.pth' --exclude '*_optim.pth' s3://${s3_bucket}/${owner}/models/${project}/${experiment}/${model}/ ${experiment}/models/${model}/
 
 ls -al
 mkdir -p tmp
