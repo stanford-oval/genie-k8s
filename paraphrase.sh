1551e36f94798b97156ae228a01e60fd74c5ac19
Sina <s.j.semnani@gmail.com>
2020-10-07 23:49:41 -0700

    Shorten paraphrase job names

--- paraphrase.sh
+++ paraphrase.sh
@@ -9,7 +9,7 @@ shift $n
 check_config "S3_BUCKET OWNER DATASET_OWNER IMAGE PROJECT TRAIN_TASK_NAME"
 
 
-JOB_NAME=${OWNER}-paraphrase-${experiment}-${output_dataset}
+JOB_NAME=${OWNER}-para-${experiment}-${output_dataset}
 cmdline="--s3_bucket ${S3_BUCKET} --owner ${OWNER} --dataset_owner ${DATASET_OWNER} --project ${PROJECT} --experiment $experiment \
          --input_dataset $input_dataset --output_dataset $output_dataset --filtering_model $filtering_model \
          --paraphrasing_model_full_path $paraphrasing_model_full_path --skip_generation $skip_generation \
