b51800412d6ae89961a04e2e5e4a304f9fb2bbef
mehrad <mehrad@stanford.edu>
2021-04-05 12:31:41 -0700

    Switch to bootleg 1.0.1

--- pipelines/common.py
+++ pipelines/common.py
@@ -39,7 +39,7 @@ WORKDIR_VERSION = '1efc491d27c756967890261c6d045fff0755450b'
 GENIE_WORKDIR_REPO = 'git@github.com:stanford-oval/genie-workdirs.git'
 GENIE_WORKDIR_VERSION = 'master'
 PARAPHRASING_MODEL = 's3://geniehai/sinaj/models/schemaorg/paraphrase/bart-large-speedup-megabatch-5m/'
-S3_DATABASE_DIR = 's3://geniehai/mehrad/extras/bootleg_files'
+S3_DATABASE_DIR = 's3://geniehai/mehrad/extras/bootleg_files_v1.0.0'
 
 # name of a secret in Kubernetes containing the SSH credentials (GitHub deploy key)
 SSH_VOLUME = 'ssh-secrets-k425k8d8h8'
