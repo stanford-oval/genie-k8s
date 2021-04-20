0f72bf45e4318649cf79feb6cf3a23caa0246f56
mehrad <mehrad@stanford.edu>
2021-04-19 17:12:41 -0700

    Add bootleg_train_predict_small_pipeline

--- pipelines/predicting.py
+++ pipelines/predicting.py
@@ -2,7 +2,7 @@ from kfp import dsl
 from kfp import components
 from kubernetes.client import V1Toleration
 
-from . import train_step
+from . import train_step, split_bootleg_merge_step
 from .common import *
 
 
@@ -106,8 +106,99 @@ def prediction_step_small(
 
 
 @dsl.pipeline(
+    name='Bootleg, train, and prediction pipeline',
+    description='Bootleg the dataset, train a model and do prediction'
+)
+def bootleg_train_predict_small_pipeline(
+        owner,
+        project,
+        experiment,
+        model,
+        task_name,
+        s3_datadir,
+        s3_bucket='geniehai',
+        s3_database_dir='None',
+        s3_bootleg_subfolder='None',
+        model_type='',
+        image=default_image,
+        genienlp_version='',
+        load_from='None',
+        valid_set='eval',
+        eval_sets='eval',
+        dataset_subfolder='None',
+        train_languages='en',
+        eval_languages='en',
+        file_extension='tsv',
+        remove_original='false',
+        skip_tensorboard='false',
+        train_iterations='',
+        train_additional_args='',
+        bootleg_model='None',
+        bootleg_additional_args='',
+        val_batch_size='1000',
+        pred_additional_args='--evaluate valid --overwrite'
+):
+    
+    s3_bootleg_prepped_data = split_bootleg_merge_step(
+        image=image,
+        owner=owner,
+        project=project,
+        experiment=experiment,
+        task_name=task_name,
+        s3_datadir=s3_datadir,
+        s3_bucket=s3_bucket,
+        s3_database_dir=s3_database_dir,
+        s3_bootleg_subfolder=s3_bootleg_subfolder,
+        genienlp_version=genienlp_version,
+        bootleg_model=bootleg_model,
+        train_languages=train_languages,
+        eval_languages=eval_languages,
+        eval_set=valid_set,
+        file_extension=file_extension,
+        remove_original=remove_original,
+        bootleg_additional_args=bootleg_additional_args
+    )
+    
+    train_op = train_step(
+        image=image,
+        owner=owner,
+        project=project,
+        experiment=experiment,
+        genienlp_version=genienlp_version,
+        model=model,
+        task_name=task_name,
+        valid_set=valid_set,
+        s3_datadir=s3_datadir,
+        s3_bucket=s3_bucket,
+        s3_database_dir=s3_database_dir,
+        load_from=load_from,
+        dataset_subfolder=dataset_subfolder,
+        skip_tensorboard=skip_tensorboard,
+        train_iterations=train_iterations,
+        s3_bootleg_prepped_data=s3_bootleg_prepped_data,
+        additional_args=train_additional_args
+    )
+    
+    pred_op = prediction_step_small(
+        image=image,
+        owner=owner,
+        genienlp_version=genienlp_version,
+        task_name=task_name,
+        eval_sets=eval_sets,
+        model_name_or_path=train_op.outputs['s3_model_dir'],
+        s3_input_datadir=s3_datadir,
+        s3_database_dir=s3_database_dir,
+        s3_bootleg_prepped_data=s3_bootleg_prepped_data,
+        model_type=model_type,
+        dataset_subfolder=dataset_subfolder,
+        val_batch_size=val_batch_size,
+        additional_args=pred_additional_args,
+    )
+
+
+@dsl.pipeline(
     name='Train and prediction pipeline',
-    description='Train a model and do prediction with it'
+    description='Train a model and do prediction'
 )
 def train_predict_small_pipeline(
         owner,
